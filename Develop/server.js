93648d9bf34e6bae115a611c5d07a63718bb78d3
PickleRick <bmkersey@gmail.com>
2022-06-19 15:03:57 -0400

    added startup for server, as well as routes for notes

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const htmlRoutes = require('./routes/html-routes');
+const apiRoutes = require('./routes/api-routes');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})
